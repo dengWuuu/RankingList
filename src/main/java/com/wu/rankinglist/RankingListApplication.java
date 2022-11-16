8f69c1810cd7bbf843c30958ef311e87b2e7efcd
Jackson_Wu <dengwu.wu@foxmail.com>
2022-11-16 16:35:25 +0800

    项目初始化

---
+++ src/main/java/com/wu/rankinglist/RankingListApplication.java
@@ -0,0 +1,13 @@
+package com.wu.rankinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RankingListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RankingListApplication.class, args);
+    }
+
+}
